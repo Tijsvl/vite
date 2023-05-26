f65175ab5c2063911a2a539e434ab053bb0b674a
Tijs <tijs.vanleur@gxsoftware.com>
2023-05-26 16:00:07 +0200

    Added SCSS and changed file extensions to .scss and .ts

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,22 +1,24 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass')
+        }
+      }
+    }
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "staticDirs": [
-    "../assets",
-    {from: "../public/rel/fonts", to: "fonts"},
-    {from: "../public/rel/icons", to: "icons"}
-  ],
-  "framework": {
-    "name": "@storybook/web-components-vite",
-    "options": {}
+  staticDirs: ['../assets', { from: '../public/rel/fonts', to: 'fonts' }, { from: '../public/rel/icons', to: 'icons' }],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {}
   },
-  "docs": {
-    "autodocs": "tag"
+  docs: {
+    autodocs: 'tag'
   }
 }
